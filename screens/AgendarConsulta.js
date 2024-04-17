63019a37862c6a335a7d5cf8956dcf0954fc40ea
David Alexandre da Silva Abreu <a56549@alunos.ipb.pt>
2024-04-17 16:58:35 +0100

    calendar added

--- screens/AgendarConsulta.js
+++ screens/AgendarConsulta.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Calendar } from 'react-native-calendars';
 
 export default function AgendarConsulta({route}) {
 
     const selectedItems = route.params.selectedItems;
-    
+    const diaData = new Date();
+    const [showLeftArrow, setShowLeftArrow] = useState(true);
+
     return (
         <View>
-            <Text>AgendarConsulta  {selectedItems.title}</Text>
+            <Calendar
+                style={styles.calendar}
+                onDayPress={(date) => {
+                    console.log(date);
+                    console.log(diaData);
+                    // setDate para algo
+                }}
+                minDate={diaData}
+                hideExtraDays={true}
+                disableArrowLeft={showLeftArrow}
+                onPressArrowRight={() => setShowLeftArrow(true)}
+                onPressArrowLeft={() => setShowLeftArrow(false)} 
+            />
         </View>
-    )
+    );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    calendar:{
+        borderRadius:10,
+        elevation:4,
+        margin:40
+    }
+})
