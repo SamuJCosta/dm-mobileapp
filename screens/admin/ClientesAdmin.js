866b7a639a5e01843f40ada2c4e6520b1d206dc6
Jorge Samuel Carneiro Costa <a56575@alunos.ipb.pt>
2024-05-08 11:48:29 +0100

    Admin Going Hard

--- screens/admin/ClientesAdmin.js
+++ screens/admin/ClientesAdmin.js
@@ -1,14 +1,105 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Image, Pressable } from "react-native";
+import { PencilIcon, PlusIcon, TrashIcon, UserIcon } from "react-native-heroicons/outline";
+import { useNavigation } from "@react-navigation/native";
 
 const ClientesAdmin = () => {
-    return (
-        <View>
-            <Text>ClientesAdmin</Text>
+  const navigation = useNavigation();
+  const firstuser = require("../../assets/1User.png");
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.block}>
+        <View style={styles.cliente}>
+          <UserIcon color={"black"} width={30} height={30} marginTop={15}/>
+          <Text style={styles.texto}>Clientes</Text>
+          <TouchableOpacity 
+            style={styles.button}
+            onPress={() => navigation.navigate("AdicionarCliente")}
+          >
+            <PlusIcon color={"white"}/>
+            <Text style={styles.buttonText}>Adicionar Cliente</Text>
+          </TouchableOpacity>
         </View>
-    )
-}
+        <View style={styles.firstuser}>
+          <Image source={firstuser}/>
+          <View style={styles.infos} >
+          <Text style={styles.name}>Jonh Dee</Text>
+          <Text style={styles.email}>john.doe@gmail.com</Text>
+          </View>
+          <Pressable onPress={()=> console.log("Apagado!")}>
+          <TrashIcon color={"gray"} marginTop={10}/>
+          </Pressable>
+          <Pressable onPress={()=> navigation.navigate('EditarCliente')}>
+          <PencilIcon color={"gray"} marginLeft={40} marginTop={10}/>
+          </Pressable>
+          </View>
+      </View>
+    </View>
+  );
+};
 
-export default ClientesAdmin
+export default ClientesAdmin;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  block: {
+    width: "95%",
+    height: "97%",
+    backgroundColor: "#fff",
+    borderRadius: 35,
+    position: "absolute",
+    marginTop: 10,
+    alignItems: "center",
+  },
+  cliente: {
+    flexDirection: "row",
+    marginTop: 10
+  },
+  texto:{
+    marginTop:18, 
+    marginLeft:10,
+    fontSize:18,
+    color:"#6FC4CF",
+    fontWeight:"600"
+  },
+  button: {
+    flexDirection:"row",
+    display: "flex",
+    width: 180,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    backgroundColor: "#6FC4CF",
+    marginTop: 8,
+    marginLeft:50,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 14,
+    fontWeight: "400",
+  },
+  firstuser:{
+    flexDirection:"row",
+    marginTop:25,
+  },
+  infos:{
+    flexDirection:"column",
+    marginRight:35
+  },
+  name: {
+    marginLeft:10,
+    fontSize: 16,
+    fontWeight: "500",
+    marginBottom: 5,
+  },
+  email: {
+    marginLeft:10,
+    fontSize: 16,
+    fontWeight: "300",
+  },
+});
